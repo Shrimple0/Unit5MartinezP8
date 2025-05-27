980dc7c8e62a01650c78fa55ab5e252f3da9e533
Shrimple0 <756116@student.dusd.net>
2025-05-27 15:25:55 -0700

    finished base game

--- Unit5MartinezP8/Assets/Scripts/GameManager.cs
+++ Unit5MartinezP8/Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI ScoreText;
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
     // Start is called before the first frame update
     void Start() 
     { 
@@ -20,10 +24,21 @@ public class GameManager : MonoBehaviour
      UpdateScore(0);
         isGameActive = true;
     }
+    
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+    }
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
     IEnumerator SpawnTarget()
     {
@@ -39,6 +54,11 @@ public class GameManager : MonoBehaviour
         score += scoreToAdd;
         ScoreText.text = "Score: " + score;
     }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     // Update is called once per frame
     void Update()
     {
